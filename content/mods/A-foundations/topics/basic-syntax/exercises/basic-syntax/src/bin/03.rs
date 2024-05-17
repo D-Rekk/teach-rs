bcfb45985e18acd89783d1fb82b22f41b51b1a97
D-Rekk <andrea_derek@hotmail.it>
2024-05-17 23:46:38 +0200

    [DONE] complete A_basic-syntax

--- content/mods/A-foundations/topics/basic-syntax/exercises/basic-syntax/src/bin/03.rs
+++ content/mods/A-foundations/topics/basic-syntax/exercises/basic-syntax/src/bin/03.rs
@@ -1,7 +1,20 @@
+use std::i32::MAX;
+
 fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
 
     // TODO
+    let mut max = 0;
+    let mut min = MAX;
+    for number in input {
+        println!("{}", number);
+        if number > max {
+            max = number
+        }
+        if number < min {
+            min = number
+        }
+    }
 
-    println!("{} is largest and {} is smallest");
+    println!("{} is largest and {} is smallest", max, min);
 }
